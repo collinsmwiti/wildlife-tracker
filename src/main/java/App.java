a754ee269f759c28a2ab248bd193875c55253476
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-17 15:54:06 +0300

    hosting to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,15 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") !=null) {
+      port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+      port = 4567;
+    }
+
+    setPort(port);
     model = new HashMap<String, Object>();
 
     before((request, response) -> {
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,30 @@
-// imports
+//imports
 import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-//connecting to postgresql database in class DB
+//class DB
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "collins", "password");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife-tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "collins" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "password" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+        }
+    }
 }
